cc04ecf8f538c65fb0dc63bdf39b8b766224e009
Daniel Rodriguez <daniel.andres.rodriguez@gmail.com>
2022-09-19 14:22:32 -0700

    Add all player list and player profile pages.

--- main.py
+++ main.py
@@ -1,10 +1,25 @@
-from flask import Flask
+import requests
+import json
+from flask import Flask, render_template, request, Response
+from utils import *
 
 app = Flask(__name__)
 
-@app.route("/")
+@app.route('/player_profile/<int:player_id>', methods=['GET'])
+def player_profile(player_id):
+    """Player profile"""
+    player_common_info, player_headline_stats = get_player_common_info(player_id)
+    return render_template('player_profile.html',
+                           player_common_info=player_common_info,
+                           player_headline_stats=player_headline_stats)
+
+
+@app.route('/', methods=['GET', 'POST'])
 def index():
-    return "Congratulations, it's a web app!"
+    """"""
+    players_list = get_player_list()
+    return render_template('index.html',
+                           players_list=players_list)
 
 
 if __name__ == "__main__":
