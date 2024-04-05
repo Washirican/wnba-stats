3d5b887cfc1e061a44a3d56d455ee8c35d8d1bc5
Daniel A. Rodriguez <daniel.andres.rodriguez@gmail.com>
2024-04-05 14:09:48 -0700

    Add code to create DB, tables, and insert values.

--- main.py
+++ main.py
@@ -3,6 +3,7 @@
 Plot WNBA Shot Charts.
 """
 
+from matplotlib.backend_tools import ToolSetCursor
 from tabulate import tabulate
 
 from utils import Game, Player, Team
@@ -40,32 +41,69 @@ logging.basicConfig(level=logging.DEBUG,
                     format='%(levelname)s: %(asctime)s - %(message)s',
                     datefmt='%d-%b-%y %H:%M:%S')
 
-
-
-
 player_name_input = 'Loyd, Jewell' #input('Enter player name (Last, First): ')
 r = requests.get(PLAYER_INDEX_URL, timeout=10)
 
 # LEARN (2024-04-05): Explore request response and determine tables and columns for DB
-all_players = json.loads(r.content.decode()[17:-1])['data']['players']
+all_players = json.loads(r.content.decode()[17:-1])
 
+for k, v in all_players['data'].items():
+    print(f'Key: {k}\nValue:{v}\n')
+
+for item in all_players['data']['teams']:
+    print(item)
+
+
+# LEARN (2024-04-05): ==================================================
 
 
 # Save data to SQLite3 Database
-conn = sqlite3.connect('mydata.db')
+# TODO (2024-04-05): Check if database exists before creating it
+conn = sqlite3.connect('wnba_data.db')
 cur = conn.cursor()
 
 # FIXME (2024-04-05): Create DB tables based on request data
-# Create a table (replace with your actual column names and data types)
-cur.execute('CREATE TABLE players (node_id INTEGER, timestamp DATETIME, price FLOAT)')
+# Create a table for general data set information
+# TODO (2024-04-05): Check if table exist before creating it
+cur.execute('CREATE TABLE data_info (generated DATETIME, seasons_count INTEGER, teams_count INTEGER, players_count INTEGER)')
+# Insert data into the general data information table
+cur.execute('INSERT INTO data_info VALUES (?, ?, ?, ?)', (all_players['generated'], all_players['seasons_count'], all_players['teams_count'], all_players['players_count']))
 
-# FIXME (2024-04-05): Create table columns and insert data based on request response.
-# Insert data into the table (replace with your actual data)
-cur.execute('INSERT INTO players VALUES (?, ?, ?)', (1, '2024-04-05 14:30:00', 0.123))
+# TODO (2024-04-05): Create table for Season data
+# TODO (2024-04-05): Check if table exist before creating it
+
+# TODO (2024-04-05): Insert data into the Seasons data table
+
+# Create table for Teams data
+# FIXME (2024-04-05): Add list of team colors to DB
+# TODO (2024-04-05): Check if table exist before creating it
+cur.execute('CREATE TABLE teams (team_id INTEGER, team_initials STRING, team_name_lower STRING, team_city STRING, team_name_title STRING, conference INTEGER, unknown_0 INTEGER, unknown_1 INTEGER, last_season INTEGER, unknown_2 INTEGER)')
+
+# TODO (2024-04-05): Insert data into the Teams data table
+for team in all_players['data']['teams']:
+    cur.execute('INSERT INTO teams VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?)', (team[0], team[1], team[2], team[3], team[4], team[5], team[6], team[7], team[8], team[9]))
+
+# Create table for Player data
+# TODO (2024-04-05): Check if table exist before creating it
+cur.execute('CREATE TABLE players (player_id INTEGER, player_name STRING, active_flag INTEGER, rookie_season INTEGER, last_season INTEGER, unknown INTEGER, current_team STRING)')
+
+# Insert data into the Players data table
+for player in all_players['data']['players']:
+    cur.execute('INSERT INTO players VALUES (?, ?, ?, ?, ?, ?, ?)', (player[0], player[1], player[2], player[3], player[4], player[5], player[6]))
 
 # Commit changes and close the connection
 conn.commit()
 conn.close()
+
+
+
+
+
+
+
+
+
+
 
 
 # TODO (2024-04-05): Create tables/columns for Teams, Games, etc.
