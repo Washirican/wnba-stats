7897bfeb38ba2c4432d6fbbac29e472132ce2162
Daniel A. Rodriguez-Delgado <daniel.a.rodriguez-delgado@boeing.com>
2024-07-16 16:46:23 -0700

    Add method to get and save team rosters.

--- data_getter.py
+++ data_getter.py
@@ -36,7 +36,8 @@ logging.basicConfig(level=logging.DEBUG,
 def get_player_list():
     """Get players list."""
     r = requests.get(PLAYER_INDEX_URL, timeout=10)
-    return json.loads(r.content.decode()[17:-1])  # ['data']['players']
+    result = json.loads(r.content.decode()[17:-1])  # ['data']['players']
+    return result
 
 
 def get_teams_list():
