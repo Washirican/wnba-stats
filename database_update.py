65e4380d65fdde61ea20beeb7af51e4c948d4ccc
Daniel A. Rodriguez <daniel.andres.rodriguez@gmail.com>
2024-07-24 17:57:32 -0700

    Add code to request team and player logs from past seasons.

--- database_update.py
+++ database_update.py
@@ -18,16 +18,15 @@ logging.basicConfig(level=logging.DEBUG,
 
 if __name__ == '__main__':
     # Get all players list
-    # u.get_player_list()
+    u.get_player_list()
 
     # Get all teams list
-    # u.get_teams_list()
+    u.get_teams_list()
 
     # SEASON = 2000
     LEAGUE_ID = 10
 
-    for season in range(2020, 2025):
-        season = 2024
+    for season in range(2023, 2025):
         # Get team game logs
         u.get_team_game_logs(season, LEAGUE_ID)
 
@@ -41,7 +40,7 @@ if __name__ == '__main__':
         u.get_player_game_logs(season, LEAGUE_ID)
 
         # Get box scores
-        # u.get_game_box_score(season)
+        u.get_game_box_score(season)
 
         # Get Player shot chart detail data
-        # u.get_shot_chart_data(season)
+        u.get_shot_chart_data(season)
