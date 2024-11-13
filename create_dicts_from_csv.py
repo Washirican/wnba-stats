a1f8d014f759a1aeb0e31a7bef114d26728151fc
Daniel A. Rodriguez-Delgado <daniel.a.rodriguez-delgado@boeing.com>
2024-11-13 15:34:11 -0800

    Update code to get team, player and game ids from already downloaded csv files.

--- create_dicts_from_csv.py
+++ create_dicts_from_csv.py
@@ -18,7 +18,56 @@ def create_player_game_dict(filename):
     
     return dict(player_game_dict)  # Convert defaultdict to a regular dictionary
 
+
+def get_team_ids_from_csv(filename):
+    # Use defaultdict to automatically initialize lists for new player IDs
+    team_ids = []
+    # Open the CSV file and read its contents
+    with open(filename, mode='r') as file:
+        reader = csv.DictReader(file)  # Use DictReader to access columns by name
+        
+        for row in reader:
+            team_id = row['id']
+            
+            # Append game_id to the list for each unique player_id
+            team_ids.append(team_id)
+    
+    return team_ids  # Convert defaultdict to a regular dictionary
+
+
+def get_player_ids_from_csv(filename):
+    # Use defaultdict to automatically initialize lists for new player IDs
+    player_ids = []
+    # Open the CSV file and read its contents
+    with open(filename, mode='r') as file:
+        reader = csv.DictReader(file)  # Use DictReader to access columns by name
+        
+        for row in reader:
+            player_id = row['PLAYER_ID']
+            
+            # Append game_id to the list for each unique player_id
+            player_ids.append(player_id)
+    
+    return player_ids  # Convert defaultdict to a regular dictionary
+
+
+
+def get_game_ids_from_csv(filename):
+    # Use defaultdict to automatically initialize lists for new player IDs
+    game_ids = []
+    # Open the CSV file and read its contents
+    with open(filename, mode='r') as file:
+        reader = csv.DictReader(file)  # Use DictReader to access columns by name
+        
+        for row in reader:
+            game_id = row['GAME_ID']
+            
+            # Append game_id to the list for each unique player_id
+            game_ids.append(game_id)
+    
+    return list(set(game_ids))   # Convert defaultdict to a regular dictionary
+
 # Usage example
-filename = "C:\\Users\\tg715c\\Documents\\Learning\\wnba-stats\\data\\player_game_logs.csv"
-player_game_dict = create_player_game_dict(filename)
-print(player_game_dict)
+teams_game_logs_filename = "./data/teams_game_logs.csv"
+game_ids = get_game_ids_from_csv(teams_game_logs_filename)
+print(game_ids)
--- main_to_csv.py
+++ main_to_csv.py
@@ -4,28 +4,29 @@ WNBA Data
 """
 import save_shot_chart_detail_to_csv as u
 
-seasons = [2024]
+seasons = [2023]
 LEAGUE_ID = 10
 
 if __name__ == '__main__':
  # Get all players list
-    # u.get_player_list()
+    u.get_player_list()
 
     # Get all teams list
-    # u.get_teams_list()
+    u.get_teams_list()
 
-    for season in seasons:
-        # # Get team game logs
-        # u.get_team_game_logs(season, LEAGUE_ID)
+    for season in seasons:  
 
-        # # Get all team rosters
-        # u.get_team_rosters(season, LEAGUE_ID)
+        # Get all team rosters
+        u.get_team_rosters(season, LEAGUE_ID)
 
-        # # Get Player Game Log data
-        # u.get_player_game_logs(season, LEAGUE_ID)
+        # Get team game logs
+        u.get_team_game_logs(season, LEAGUE_ID)
 
-        # # Get box scores
-        # u.get_game_box_score(season)
+        # Get Player Game Log data
+        u.get_player_game_logs(season, LEAGUE_ID)
+
+        # Get box scores
+        u.get_game_box_score(season)
 
         # Get Player shot chart detail data
         u.get_shot_chart_data(season)
